739181dd32df278a4e0b7f158ebb816a32c059e1
LVvVQ <287535753@qq.com>
2022-09-11 14:48:40 +0800

    nvim: Fix dap-ui.lua and winshift.lua configuration

--- config/.config/nvim/lua/user/dap/dap-ui.lua
+++ config/.config/nvim/lua/user/dap/dap-ui.lua
@@ -4,7 +4,7 @@ if not status_ok then
   return
 end
 
-dapui.setup ({
+dapui.setup({
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
     -- Use a table to apply multiple mappings
@@ -15,25 +15,25 @@ dapui.setup ({
     repl = "r",
     toggle = "t",
   },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.35, -- Can be float or integer > 1
+  layouts = {
+    {
+      elements = {
+        'scopes',
+        'breakpoints',
+        'stacks',
+        'watches',
       },
-      { id = "stacks", size = 0.35 },
-      { id = "watches", size = 0.15 },
-      { id = "breakpoints", size = 0.15 },
+      size = 40,
+      position = 'left',
     },
-    size = 40,
-    position = "left", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = { "repl" },
-    size = 5,
-    position = "bottom", -- Can be "left", "right", "top", "bottom"
+    {
+      elements = {
+        'repl',
+        'console',
+      },
+      size = 5,
+      position = 'bottom',
+    },
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
--- config/.config/nvim/lua/user/winshift.lua
+++ config/.config/nvim/lua/user/winshift.lua
@@ -1,7 +1,7 @@
 -- Lua
 require("winshift").setup({
-  highlight_moving_win = true,  -- Highlight the window being moved
-  focused_hl_group = "Visual",  -- The highlight group used for the moving window
+  highlight_moving_win = true, -- Highlight the window being moved
+  focused_hl_group = "Visual", -- The highlight group used for the moving window
   moving_win_options = {
     -- These are local options applied to the moving window while it's
     -- being moved. They are unset when you leave Win-Move mode.
@@ -13,20 +13,19 @@ require("winshift").setup({
   -- The window picker is used to select a window while swapping windows with
   -- ':WinShift swap'.
   -- A string of chars used as identifiers by the window picker.
-  window_picker_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-  window_picker_ignore = {
-    -- This table allows you to indicate to the window picker that a window
-    -- should be ignored if its buffer matches any of the following criteria.
-    filetype = {  -- List of ignored file types
+  picker_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+  filter_rules = {
+    cur_win = true,
+    floats = true,
+    filetype = {
       "NvimTree",
     },
-    buftype = {   -- List of ignored buftypes
+    buftype = {
       "terminal",
       "quickfix",
     },
-    bufname = {   -- List of regex patterns matching ignored buffer names
+    bufname = {
       [[.*foo/bar/baz\.qux]]
     },
   },
 })
-
