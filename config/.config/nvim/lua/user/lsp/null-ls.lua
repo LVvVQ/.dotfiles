d5fc8d278b43ece7fea4f905eb4245287ea29f40
LVvVQ <287535753@qq.com>
2023-03-05 18:17:01 +0800

    nvim: Turn off auto-format on save

--- config/.config/nvim/lua/user/lsp/null-ls.lua
+++ config/.config/nvim/lua/user/lsp/null-ls.lua
@@ -17,15 +17,15 @@ local sources = {
 	code_actions.eslint_d,
 }
 
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
+--[[ local lsp_formatting = function(bufnr) ]]
+--[[ 	vim.lsp.buf.format({ ]]
+--[[ 		filter = function(client) ]]
+--[[ 			-- apply whatever logic you want (in this example, we'll only use null-ls) ]]
+--[[ 			return client.name == "null-ls" ]]
+--[[ 		end, ]]
+--[[ 		bufnr = bufnr, ]]
+--[[ 	}) ]]
+--[[ end ]]
 
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -37,7 +37,7 @@ local on_attach = function(client, bufnr)
 			group = augroup,
 			buffer = bufnr,
 			callback = function()
-				lsp_formatting(bufnr)
+				--[[ lsp_formatting(bufnr) ]]
 			end,
 		})
 	end
