dfe915883ed0c2af86133e7c83046c722c7ad978
LVvVQ <287535753@qq.com>
2023-07-22 17:28:52 +0800

    nvim: Fix neo-tree configuration

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
@@ -7,31 +8,33 @@ return {
   },
   cmd = "Neotree",
   keys = {
-    { "tt", "<cmd>NeoTreeRevealToggle<cr>", desc = "NeoTree" },
+    { "tt", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
   },
   config = function()
-    require("neo-tree").setup {
+    require("neo-tree").setup({
       close_if_last_window = true,
       window = {
         position = "left",
         width = 30,
-        filesystem = {
-          bind_to_cwd = false,
-          follow_current_file = true,
-        },
         mappings = {
           ["o"] = "open_tabnew",
           ["t"] = "none"
         }
       },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+      },
       default_component_configs = {
         indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          with_expanders = true,
           expander_collapsed = "",
           expander_expanded = "",
           expander_highlight = "NeoTreeExpander",
         },
       },
-    }
+    })
   end,
 }
